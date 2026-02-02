5079aaf25a9f5e4a9f8a416dcbcd5486ad460c30
kranzowe <kranzowe@gmail.com>
2026-02-01 19:24:27 -0700

    idk still far off

---
+++ hw/2-Markov-Decision-Processes/hw2_solutions.jl
@@ -0,0 +1,109 @@
+using DMUStudent.HW2
+using POMDPs: states, actions
+using POMDPTools: ordered_states, render
+import Cairo, Fontconfig # Needed in some cases for rendering the value function on grid world
+using Debugger
+using StaticArrays
+##############
+# Instructions
+##############
+#=
+
+This starter code is here to show examples of how to use the HW2 code that you
+can copy and paste into your homework code if you wish. It is not meant to be a
+fill-in-the blank skeleton code, so the structure of your final submission may
+differ from this considerably.
+
+=#
+
+############
+# Question 3
+############
+
+@show actions(grid_world) # prints the actions. In this case each action is a Symbol. Use ?Symbol to find out more.
+
+T = transition_matrices(grid_world)
+display(T) # this is a Dict that contains a transition matrix for each action
+
+@show T[:left][1, 2] # the probability of transitioning between states with indices 1 and 2 when taking action :left
+
+R = reward_vectors(grid_world)
+display(R) # this is a Dict that contains a reward vector for each action
+
+@show R[:right][1] # the reward for taking action :right in the state with index 1
+
+function value_iteration(m)
+    # It is good to put performance-critical code in a function: https://docs.julialang.org/en/v1/manual/performance-tips/
+    A = actions(m)
+    T_sparse = transition_matrices(grid_world, sparse=true)
+    @show T_sparse
+    states_defined = states(m)
+    num_states = length(states_defined)
+    V_old = zeros(num_states)
+    V_new = zeros(num_states)
+
+    while true
+        for i in 1:num_states
+            state = states_defined[i]
+            this_states_T = T_sparse[i]
+            possible_next_
+            Q_new[i] = R
+            
+            Q_1
+            
+            max()
+        end
+
+        # inf norm
+        if maximum(abs.(V_new .- V)) < 1e-8
+            break
+        end
+
+    return V
+end
+
+#@enter(value_iteration(grid_world))
+
+V = rand(length(states(grid_world)))*10.0 # replace this with value_iteration(m)
+# If you are in an environment with multimedia capability (e.g. VSCode, Jupyter, Pluto), use this:
+#display(render(grid_world, color=V)) # In the REPL, this will output an annoying amount of text
+# If you are in the REPL or want to save a png, use this:
+# using Compose: draw, PNG
+# draw(PNG("value.png"), render(grid_world, color=V))
+
+############
+# Question 4
+############
+
+# You can create an mdp object representing the problem with the following:
+m = UnresponsiveACASMDP(2)
+
+# transition_matrices and reward_vectors work the same as for grid_world, however this problem is much larger, so you will have to exploit the structure of the problem. In particular, you may find the docstring of transition_matrices helpful:
+# display(@doc(transition_matrices))
+
+V = value_iteration(m)
+
+@show HW2.evaluate(V)
+
+########
+# Extras
+########
+
+# The comments below are not needed for the homework, but may be helpful for interpreting the problems or getting a high score on the leaderboard.
+
+# Both UnresponsiveACASMDP and grid_world implement the POMDPs.jl interface. You can find complete documentation here: https://juliapomdp.github.io/POMDPs.jl/stable/api/#Model-Functions
+
+# To convert from physical states to indices in the transition function, use the stateindex function
+# IMPORTANT NOTE: YOU ONLY NEED TO USE STATE INDICES FOR THIS ASSIGNMENT, using the states may help you make faster specialized code for the ACAS problem, but it is not required
+# using POMDPs: states, stateindex
+
+# s = first(states(m))
+# @show si = stateindex(m, s)
+
+# # To convert from a state index to a physical state in the ACAS MDP, use convert_s:
+# using POMDPs: convert_s
+
+# @show s = convert_s(ACASState, si, m)
+
+# # To visualize a state in the ACAS MDP, use
+# render(m, (s=s,))
