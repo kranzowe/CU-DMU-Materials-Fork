b3d5961b73ece86de4bd77ca6e3df416083e0ed9
kranzowe <kranzowe@gmail.com>
2026-02-17 14:48:32 -0700

    we gottem

--- hw/3-Advanced-MDPs/threaded_solver.jl
+++ hw/3-Advanced-MDPs/threaded_solver.jl
@@ -103,7 +103,7 @@ function POMDPs.action(policy::ThreadedSolverPolicy, s)
     start = time_ns()
 
     Threads.@threads for tid in 1:policy.num_threads
-        while time_ns() < start + 15_000_000
+        while time_ns() < start + 32_000_000
             threaded_solver_simulate!(policy, s, tid)
         end
     end
